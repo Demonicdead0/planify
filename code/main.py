c21b0c9f0c6e088628d5d7ea77df2206a354298f
unknown <jhamsidrbz@gmail.com>
2024-10-22 14:56:51 -0500

    I add a new code

--- code/main.py
+++ code/main.py
@@ -10,10 +10,28 @@ import json
 title("Planify")
 
 # verify if the datas exists 
+routetask: str = '../data/task.json'
+routeproject: str = '../data/project.json'
+routepriority: str = '../data/priority.json'
+
+d_task: json
+d_project: json
+d_priority: json
+
+with open(routetask, encoding='utf8') as tasks:
+    d_task = json.load(tasks)
+
+with open(routeproject, encoding='utf8') as projects:
+    d_project = json.load(projects)
+
+with open(routepriority, encoding='utf8') as priorities:
+    d_priority = json.load(priorities)
+print(d_task)
+
 alert("no existe archivos json")
 
 # execute a loop
 while True:
     subtittle("Task")
-    command: str = len_command()
+    command: str = len_command() 
     pass
--- code/module/commands.py
+++ code/module/commands.py
@@ -1,3 +1,9 @@
+import json
+
+route = "../../data/data.json"
+
+data: json = json.load(route)
+
 def get_command() -> list:
     element: list = []
     element.append('add')
@@ -14,3 +20,28 @@ def len_command() -> str:
         if elemen == element:
             return element
     return "error"
+
+def get_command(command: str):
+    element: dict = dict()
+    if command == 'add':
+        expression: str = command[3:]
+        pos: int = 0
+        while pos < (len(expression) - 2):
+            if expression[pos] == expression[pos+1] == "-":
+                littlecommand: str = ""
+                pos += 2
+                while pos < (len(expression) - 2) and expression[pos] != ':':
+                    littlecommand += expression[pos]
+                    pos += 1
+                pos += 1
+                text: str = ""
+                while (pos < (len(expression) - 2) and expression[pos] != '-'):
+                    text += expression[text]
+                    pos += 1
+                pos -= 1
+                if littlecommand == 'm:':
+                    element['idtask'] = data['nro_task']
+                    
+
+
+            pos += 1
